27166d18442a26d24c275bc07b13dae83b048caf
Ayush <ayushganguli1769@gmail.com>
2020-09-24 03:47:22 +0530

    added zack and hankerchiefs

--- maths/hankies.py
+++ maths/hankies.py
@@ -1,3 +1,11 @@
-t= int(input())
+def gcd(m,n):
+    if m < n:
+        (m,n) = (n,m)
+    if m % n == 0:
+        return n
+    else:
+        return gcd(m%n,n)
+t = int(input())
 for _ in range(t):
-    
+    (l,b) = map(int,input().strip().split())
+    print(gcd(l,b))
